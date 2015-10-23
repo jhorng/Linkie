633ed736b2e9b9e3d5a7108d7648908c500844d1
Chu Jaan Horng <jjaanhorng@gmail.com>
2015-10-23 17:29:24 +0800

    PG13, PG14, PB13 & PC5 are able to light up

--- app/GPIO.c
+++ app/GPIO.c
@@ -1,29 +1,55 @@
 #include "GPIO.h"
 #include "stm32f4xx_hal_rcc.h"
 
-void configurePin(int direction, int pinNumber, GPIO_TypeDef *port){
+void configurePin(int mode, int pinNum, GPIO *port){
 	GPIO_InitTypeDef gpio;
 
 	__HAL_RCC_GPIOG_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
 
+	port->MODER &= ~(3 << (pinNum * 2));
+	port->MODER |= mode << (pinNum * 2);
+	port->OTYPER &= ~(1 << pinNum);
+	port->OSPEED &= ~(3 << (pinNum * 2));
+	port->OSPEED |= GPIO_HIGH_SPEED << (pinNum * 2);
 
+	//PORTB->MODER = 0x04000000;
+	//PORTB->OTYPER = 0;
+	//PORTB->OSPEED = 0x20000000;
 
-	int *ptr = (int *)0x40021800;	//GPIOG
-	int *ptrB = (int *)0x40020400;	//GPIOB
-	int *ptrC = (int *)0x40020800;	//GPIOC
-	ptr[0] = 0x14000000;		// MODE.pin14 = output
-	ptr[1] = 0;					// OTYPE.pin14 = push-pull
-	ptr[2] = 0x20000000;		// OSPEED.pin14 = high speed
+	//PORTC->MODER = 0x00000400;
+	//PORTC->OTYPER = 0;
+	//PORTC->OSPEED = 0x20000000;
 
-	ptrB[0] = 0x04000000;		// MODE.pin13 = output
-	ptrB[1] = 0;					// OTYPE.pin13 = push-pull
-	ptrB[2] = 0x20000000;		// OSPEED.pin13 = high speed
+	/*
+	PORTG->MODER = 0x14000000;
+	PORTG->OTYPER = 0;
+	PORTG->OSPEED = 0x20000000;
 
-	ptrC[0] = 0x00000400;		// MODE.pin5 = output
-	ptrC[1] = 0;				// OTYPE.pin5 = push-pull
-	ptrC[2] = 0x20000000;		// OSPEED.pin5 = high speed
+	PORTB->MODER = 0x04000000;
+	PORTB->OTYPER = 0;
+	PORTB->OSPEED = 0x20000000;
+
+	PORTC->MODER = 0x00000400;
+	PORTC->OTYPER = 0;
+	PORTC->OSPEED = 0x20000000;
+	*/
+
+	//int *ptr = (int *)0x40021800;	//GPIOG
+	//ptr[0] = 0x14000000;		// MODE.pin14 = output
+	//ptr[1] = 0;					// OTYPE.pin14 = push-pull
+	//ptr[2] = 0x20000000;		// OSPEED.pin14 = high speed
+
+	//int *ptrB = (int *)0x40020400;	//GPIOB
+	//ptrB[0] = 0x04000000;		// MODE.pin13 = output
+	//ptrB[1] = 0;				// OTYPE.pin13 = push-pull
+	//ptrB[2] = 0x20000000;		// OSPEED.pin13 = high speed
+
+	//int *ptrC = (int *)0x40020800;	//GPIOC
+	//ptrC[0] = 0x00000400;		// MODE.pin5 = output
+	//ptrC[1] = 0;				// OTYPE.pin5 = push-pull
+	//ptrC[2] = 0x20000000;		// OSPEED.pin5 = high speed
 
 /*
 	gpio.Pin = pinNumber;
@@ -34,10 +60,10 @@ void configurePin(int direction, int pinNumber, GPIO_TypeDef *port){
 }
 
 
-void writeOne(int pinNumber, GPIO_TypeDef *port){
-	HAL_GPIO_WritePin(port, pinNumber, GPIO_PIN_SET);
+void writeOne(int pinNum, GPIO_TypeDef *port){
+	HAL_GPIO_WritePin(port, pinNum, GPIO_PIN_SET);
 }
 
-void writeZero(int pinNumber, GPIO_TypeDef *port){
-	HAL_GPIO_WritePin(port, pinNumber, GPIO_PIN_RESET);
+void writeZero(int pinNum, GPIO_TypeDef *port){
+	HAL_GPIO_WritePin(port, pinNum, GPIO_PIN_RESET);
 }
